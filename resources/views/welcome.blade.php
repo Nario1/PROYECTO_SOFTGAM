fc90a1a75ed1b92bd81674c672e14f31e9773d56
Nario1 <71917543@continental.edu.pe>
2025-10-13 15:02:03 -0500

    blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,34 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-
-    <!-- Favicon -->
     <link rel="shortcut icon" href="/img/logo.jpg" type="image/png" />
-
     <title>SOFTWARE GAMIFICADO</title>
-
-    @php
-        $manifestPath = public_path('build/manifest.json');
-        $manifest = file_exists($manifestPath) ? json_decode(file_get_contents($manifestPath), true) : [];
-    @endphp
-
-    {{-- Cargar todos los CSS del build --}}
-    @foreach($manifest as $file)
-        @if(isset($file['css']))
-            @foreach($file['css'] as $cssFile)
-                <link rel="stylesheet" href="{{ asset('build/' . $cssFile) }}">
-            @endforeach
-        @endif
-    @endforeach
+    @vite("resources/js/app.js")
 </head>
 <body>
     <div id="root"></div>
-
-    {{-- Cargar JS de entrada --}}
-    @foreach($manifest as $file)
-        @if(isset($file['isEntry']) && $file['isEntry'] === true && isset($file['file']))
-            <script type="module" src="{{ asset('build/' . $file['file']) }}"></script>
-        @endif
-    @endforeach
 </body>
 </html>
